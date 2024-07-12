903c37949a4a7de1ad6d2a14b6db5a1e657d095c
hiranomami <m.hirano.sys23@morijyobi.ac.jp>
2024-07-12 11:48:33 +0900

    ユーザー入力で手を決める処理を実装したclose #12,close#13,close#14

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 package src;
-
+import java.io.*;
 public class Main {
 
 
@@ -14,10 +14,60 @@ public class Main {
         System.out.println("2.チョキ");
         System.out.println("3.パー");
         System.out.println("あなたの手を選択してください。>");
+        //ユーザ入力
+
+        String input = null;
+
+        try {
+
+            BufferedReader inputuser = new BufferedReader(
+
+                    new InputStreamReader(System.in)
+
+            );
+
+            input = inputuser.readLine();
+
+        } catch (IOException e) {
+
+            System.out.print("システムエラー");
+
+            System.exit(0);
+
+            return;
+
+        }
+        if (input == null) {
+            System.out.print("1-3の値を入力してください。");
+            System.exit(0);
+            return;
+        }
+
+        //ユーザの入力によりジャンケンの手をセットする
+
+        if (input.equals("1")) {
+
+            user.setHand(new Gu());
+
+        } else if (input.equals("2")) {
+
+            user.setHand(new Choki());
+
+        } else if (input.equals("3")) {
+
+            user.setHand(new Pa());
+
+        } else {
+
+            System.out.print("1-3の値を入力してください。");
+
+            System.exit(0);
+
+            return;
+
+        }
         //テストコード
-        System.out.println(user.getPlayerName());
-        com.setPlayerName("コンピュータ");
-        System.out.println(com.getPlayerName());
+        System.out.println(user.getHand().getHandName());
 
     }
 
